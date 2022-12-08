1982324acaac1d87b4f2b353aaef71eedbad2011
Kevin Stefoni <86985049+kevinStefoni@users.noreply.github.com>
2022-12-08 09:43:33 -0600

    Add files via upload

---
+++ Group14_Lab2/Lab2_Task1_14.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from __future__ import print_function 
+from __future__ import division      
+
+import time     # import the time library for the sleep function
+import brickpi3 # import the BrickPi3 drivers
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH) # Configure for a touch sensor. If an EV3 touch sensor is connected, it will be configured for EV3 touch, otherwise it'll configured for NXT touch.
+
+t_end = time.time() + 1.5
+
+try:
+    print("Press touch sensor on port 1 to run motors")
+    value = 0
+    while not value:
+        try:
+            value = BP.get_sensor(BP.PORT_1)
+        except brickpi3.SensorError:
+            pass
+    
+    speed = 0
+    adder = 1
+    while True:
+        # BP.get_sensor retrieves a sensor value.
+        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
+        # BP.get_sensor returns the sensor value.
+        try:
+            value = BP.get_sensor(BP.PORT_1)
+        except brickpi3.SensorError as error:
+            print(error)
+            value = 0
+        
+        if value == 1:                          # if the touch sensor is pressed
+            speed = 100
+            BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
+            while True:    
+                time.sleep(.5)
+                value = BP.get_sensor(BP.PORT_1)  
+                if value == 1:
+                    speed = 0
+                    BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
+                    time.sleep(.5)
+                    break
+        
+        try:
+            # Each of the following BP.get_motor_encoder functions returns the encoder value (what we want to display).
+            print("Encoder A: %6d  B: %6d  C: %6d  D: %6d" % (BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B), BP.get_motor_encoder(BP.PORT_C), BP.get_motor_encoder(BP.PORT_D)))
+        except IOError as error:
+            print(error)
+        
+        time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
+
+except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+    BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
